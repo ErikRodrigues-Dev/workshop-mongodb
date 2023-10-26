4c713aec52a90dd6c73934df7e410c63f71fdf42
ErikRodrigues-Dev <erik.rodrigues.dev@gmail.com>
2023-10-26 12:37:24 -0300

    Obtendo um usuário por id Parte 2 mais tratamento de erro

---
+++ src/main/java/com/erikrodrigues/workshopmongo/Resource/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.erikrodrigues.workshopmongo.resource.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.erikrodrigues.workshopmongo.service.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		}
+	
+}
--- src/main/java/com/erikrodrigues/workshopmongo/service/exception/ObjectNotFoundException.java
+++ src/main/java/com/erikrodrigues/workshopmongo/service/exception/ObjectNotFoundException.java
@@ -4,6 +4,7 @@ public class ObjectNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public ObjectNotFoundException(String msg) {
+		super(msg);
 		
 	}
 	
